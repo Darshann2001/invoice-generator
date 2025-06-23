a8f8d4dbc6c579b81b1ad3162d316e0c985e3053
Darshan@7784 <chavandarshan599@gmail.com>
2025-06-23 13:52:53 +0530

    minor change test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/invoice-generator/', // ✅ EXACT repo name with slashes
+  base: 'https://github.com/Darshann2001/invoice-generator/', // ✅ EXACT repo name with slashes
 })
